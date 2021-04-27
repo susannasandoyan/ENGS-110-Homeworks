7994171e974e0bace74a8afde0396e9b9d1f8173
Susanna <susanna_sandoyan@edu.aua.am>
2021-04-28 03:31:06 +0400

    homework

---
+++ Fib.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main(){
+        int age ;
+        printf ("Enter your age: ");
+        scanf ("%d", &age );
+        int f1 = 0;
+        int f2 = 1;
+        int sum = 0;
+        while (f2<=age){
+                sum = sum + f2;
+                int temp = f2;
+                f2 = f1 + f2;
+                f1 = temp;
+               
+        }
+	printf ("Summary of all fibonacci numbers less than your age: %d", sum);
+        int binary[32];
+        int i = 0;
+        while (sum > 0){
+                binary[i] = sum % 2;
+                sum = sum / 2;
+                i++;
+        }
+	printf("\nBinary representation of the sum: ");
+        for (int k = i-1; k>=0; k--){
+                printf ("%d", binary[k]);
+        }
+	printf ("\n");
+	return sum;
+      
+}
+
+
